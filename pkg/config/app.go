7bcffcced54c2cc9391b8bbfb57b093d51b044ed
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-14 13:13:17 +0530

    feat: Config

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db * gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "admin:password/simplerest?charset=utf8&parseTime=true&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+    return db
+}
