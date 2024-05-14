4c98322ba875fd184af1a57f894f5c545244bd80
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-14 14:11:32 +0530

    feat: GetBook controller

--- pkg/controllers/bookController.go
+++ pkg/controllers/bookController.go
@@ -1,1 +1,19 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Rizz-33/book-management-system/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
