59774c1f03503d39e78c17428fd2f647236f07b8
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-14 13:28:00 +0530

    feat: Model

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,45 @@
 package models
+
+import (
+	"github.com/Rizz-33/book-management-system/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func GetBooks() []Book {
+	var books []Book
+    db.Find(&books)
+    return books
+}
+
+func GetBook(id int) Book {
+	var book Book
+    db.First(&book, id)
+    return book
+}
+
+func CreateBook(book Book) {
+    db.Create(&book)
+}
+
+func UpdateBook(book Book) {
+    db.Save(&book)
+}
+
+func DeleteBook(id int) {
+    db.Delete(&Book{}, id)
+}
