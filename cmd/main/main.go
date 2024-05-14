c368aa1820d851c7eb49a1b7733a4cf38dd24cb3
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-14 13:37:18 +0530

    feat: Main func

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Rizz-33/book-management-system/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", nil))
+}
